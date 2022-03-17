c14035b9be31da45e5264e9e5217948b221e92b1
jctaylorjr <jctaylor2nd@gmail.com>
2022-03-17 19:02:03 -0400

    added the mouth arc for the smiley face

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { arc } from 'd3';
+
 const width = 960;
 const height = 500;
 const centerX = width / 2;
@@ -9,14 +11,23 @@ const eyeOffsetX = 90;
 const eyeOffsetY = 90;
 const eyeRadius = 50;
 
+const mouthArc = arc()
+  .innerRadius(90)
+  .outerRadius(100)
+  .startAngle(Math.PI * .5)
+  .endAngle(Math.PI * 1.5);
+
 const App = () => (
   <svg width={width} height={height}>
-    <circle cx={centerX} cy={centerY} r={radius} fill="gold" stroke="black" strokeWidth={strokeWidth}>
-    </circle>
-    <circle cx={centerX - eyeOffsetX} cy={centerY - eyeOffsetY} r={eyeRadius} fill="black">
-    </circle>
-    <circle cx={centerX + eyeOffsetX} cy={centerY - eyeOffsetY} r={eyeRadius} fill="black">
-    </circle>
+    <g transform={`translate(${centerX}, ${centerY})`}>
+      <circle r={radius} fill="gold" stroke="black" strokeWidth={strokeWidth}>
+      </circle>
+      <circle cx={- eyeOffsetX} cy={- eyeOffsetY} r={eyeRadius} fill="black">
+      </circle>
+      <circle cx={+eyeOffsetX} cy={- eyeOffsetY} r={eyeRadius} fill="black">
+      </circle>
+      <path d={mouthArc()} />
+    </g>
   </svg >
 );
 
