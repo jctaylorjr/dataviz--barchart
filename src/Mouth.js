45a9fd2fb359cbfd293fee8fddd5d45f6bd621bc
jctaylorjr <jctaylor2nd@gmail.com>
2022-03-18 16:31:04 -0400

    Background circle, eyes, and mouth moved to their own files and imported to App.js

---
+++ src/Mouth.js
@@ -0,0 +1,10 @@
+import { arc } from 'd3';
+
+export const Mouth = ({ mouthRadius, mouthWidth }) => {
+    const mouthArc = arc()
+        .innerRadius(mouthRadius)
+        .outerRadius(mouthWidth)
+        .startAngle(Math.PI * .5)
+        .endAngle(Math.PI * 1.5);
+    return <path d={mouthArc()} />;
+}
