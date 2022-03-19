df1d9d62708cf793ee3dd29caf99eaad8a63936f
jctaylorjr <jctaylor2nd@gmail.com>
2022-03-19 17:15:18 -0400

    deleted unused files from clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import { Face } from './Face';
-import { range } from 'd3';
+const width = 960;
+const height = 500;
+const circleX = width / 2;
+const circleY = height / 2;
 
-const width = 240;
-const height = 125;
+const App = () => (
+  <svg>
+ <circle>
 
-const array = range(4 * 4);
-
-const App = () =>
-	array.map(() => (
-		<Face
-			width={width}
-			height={height}
-			centerX={width / 2}
-			centerY={height / 2}
-			strokeWidth={1 + Math.random() * 10}
-			eyeOffsetX={9 + Math.random() * 10}
-			eyeOffsetY={9 + Math.random() * 10}
-			eyeRadius={4 + Math.random() * 10}
-			mouthRadius={2 + Math.random() * 10}
-			mouthWidth={3 + Math.random() * 10}
-		/>
-	));
+ </circle>
+  </svg>
+);
 
 export default App;
--- src/BackgroundCircle.js
+++
@@ -1,8 +0,0 @@
-export const BackgroundCircle = ({ radius, strokeWidth }) => (
-	<circle
-		r={radius}
-		fill='gold'
-		stroke='black'
-		strokeWidth={strokeWidth}
-	></circle>
-);
