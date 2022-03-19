cff91da06a7e3bf44dc1fcf5fec7b3a2413e462d
jctaylorjr <jctaylor2nd@gmail.com>
2022-03-19 17:50:52 -0400

    feature: circle will follow mouse on browser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+
 const width = 960;
 const height = 500;
-const circleX = width / 2;
-const circleY = height / 2;
+const circleRadius = 30;
+const initialMousePosition = { x: width / 2, y: height / 2 };
 
-const App = () => (
-	<svg>
-		<circle cx={circleX} cy={circleY}></circle>
-	</svg>
-);
+const handleMouseMove = (event) => {
+	const { clientX, clientY } = event;
+	console.log({ clientX, clientY });
+};
+
+const App = () => {
+	const [mousePosition, setMousePosition] = useState(initialMousePosition);
+	const handleMouseMove = (event) => {
+		const { clientX, clientY } = event;
+		setMousePosition({ x: clientX, y: clientY });
+	};
+	return (
+		<svg width={width} height={height} onMouseMove={handleMouseMove}>
+			<circle
+				cx={mousePosition.x}
+				cy={mousePosition.y}
+				r={circleRadius}></circle>
+		</svg>
+	);
+};
 
 export default App;
