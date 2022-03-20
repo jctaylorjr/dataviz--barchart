c74494510942d0c3b27d934e4cac6b8c5fdf6af2
jctaylorjr <jctaylor2nd@gmail.com>
2022-03-20 11:12:16 -0400

    got rid of pie chart code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import * as d3 from 'd3';
 import { useEffect, useState } from 'react';
-import { message } from './Message';
 
 const csvURL =
-	'https://gist.githubusercontent.com/jctaylorjr/8ca53d2c9bf976bb9f0de77eeb36881a/raw/28db9dd8dcf8aabf603bb89359edc3cae6b18366/colors.csv';
+	'https://gist.githubusercontent.com/jctaylorjr/0b1e319eb86cce1c68ad6640d681fdd5/raw/a6119e9e5a66ee4312414cfc9c9f0e63ad859f8a/united-nations-population-prospects-2019.csv';
 const width = 960;
 const height = 480;
 const centerX = width / 2;
 const centerY = height / 2;
-
-// const ColorArc = ({ mouthRadius, mouthWidth }) => {
-// 	const colorArc = arc()
-// 		.innerRadius(mouthRadius)
-// 		.outerRadius(mouthWidth)
-// 		.startAngle(Math.PI * 0.5)
-// 		.endAngle(Math.PI * 1.5);
-// 	return <path d={colorArc()} />;
-// };
-
-const colorArc = d3.arc().innerRadius(100).outerRadius(200);
 
 const App = () => {
 	const [data, setData] = useState(null);
@@ -30,27 +18,7 @@ const App = () => {
 		return <div>loading . . .</div>;
 	}
 
-	const colorPie = d3.pie().value(1);
-
-	return (
-		<svg width={width} height={height}>
-			<g transform={`translate(${centerX}, ${centerY})`}>
-				{colorPie(data).map((d) => (
-					<path fill={d.data['RGB hex value']} d={colorArc(d)} />
-				))}
-			</g>
-		</svg>
-	);
+	return <svg width={width} height={height}></svg>;
 };
-
-// {data.map((d, i) => (
-// 	<path
-// 		fill={d['RGB hex value']}
-// 		d={colorArc({
-// 			startAngle: (i / data.length) * Math.PI * 2,
-// 			endAngle: ((i + 1) / data.length) * Math.PI * 2,
-// 		})}
-// 	/>
-// ))}
 
 export default App;
--- src/Message.js
+++
@@ -1,9 +0,0 @@
-import * as d3 from 'd3';
-
-export const message = (data) => {
-	let message = '';
-	message += Math.round(d3.csvFormat(data).length / 1024) + ' kB\n';
-	message += data.length + ' rows\n';
-	message += data.columns.length + ' columns';
-	return message;
-};
