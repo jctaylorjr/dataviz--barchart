9c4f5dad1f8412b77f36778c9c2ccf9dfaa7bb88
jctaylorjr <jctaylor2nd@gmail.com>
2022-03-20 12:12:19 -0400

    bar chart added with 10 columns

--- src/App.js
+++ src/App.js
@@ -5,20 +5,45 @@ const csvURL =
 	'https://gist.githubusercontent.com/jctaylorjr/0b1e319eb86cce1c68ad6640d681fdd5/raw/a6119e9e5a66ee4312414cfc9c9f0e63ad859f8a/united-nations-population-prospects-2019.csv';
 const width = 960;
 const height = 480;
-const centerX = width / 2;
-const centerY = height / 2;
 
 const App = () => {
 	const [data, setData] = useState(null);
 	useEffect(() => {
-		d3.csv(csvURL).then(setData);
+		const row = (d) => {
+			d.Population = +d['2020'];
+			return d;
+		};
+		d3.csv(csvURL, row).then((data) => {
+			setData(data.slice(0, 10));
+		});
 	}, []);
 
 	if (!data) {
 		return <div>loading . . .</div>;
 	}
 
-	return <svg width={width} height={height}></svg>;
+	const yScale = d3
+		.scaleBand()
+		.domain(data.map((d) => d.Country))
+		.range([0, height]);
+
+	const xScale = d3
+		.scaleLinear()
+		.domain([0, d3.max(data, (d) => d.Population)])
+		.range([0, width]);
+
+	return (
+		<svg width={width} height={height}>
+			{data.map((d) => (
+				<rect
+					x={0}
+					y={yScale(d.Country)}
+					width={xScale(d.Population)}
+					height={yScale.bandwidth()}
+				/>
+			))}
+		</svg>
+	);
 };
 
 export default App;
